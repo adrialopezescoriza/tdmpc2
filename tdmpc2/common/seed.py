375ca82e83aea6e8696eb9bb10be818b45a4d2a3
adrialopezescoriza <alopez@ethz.ch>
2024-10-01 16:30:54 -0700

    Small seed buffer modification

--- tdmpc2/common/seed.py
+++ tdmpc2/common/seed.py
@@ -14,17 +14,17 @@ def set_seed(seed):
 	torch.cuda.manual_seed_all(seed)
 
 class SeedScheduler:
-	def __init__(self, enable=True, sr_thresehold=0.5, num_envs=1):
+	def __init__(self, enable=True, sr_thresehold=0.3, num_envs=1):
 		self.sr_thresehold = sr_thresehold
 		self.enable = enable
 		self.num_envs = num_envs
 		self._started = False
 		self.sr = deque([], maxlen=5)
 
-	def start(self, init_seed=0, max_seeds=1e4):
+	def start(self, init_seed=0, max_seeds=1e4, init_counter=1):
 		self.max_seeds = int(max_seeds)
 		self.seeds = np.arange(start=init_seed, stop=init_seed+max_seeds, step=self.num_envs, dtype=np.int32)
-		self.counter = 1
+		self.counter = init_counter
 		self._started = True
 	
 	def step(self, sr) -> None:
