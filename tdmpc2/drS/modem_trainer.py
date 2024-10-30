1f66100ce0c35a7d5f74b5e9b1dc9f91a36adcd1
adrialopezescoriza <alopez@ethz.ch>
2024-10-30 13:24:20 -0700

    Modem v2 trick with alpha decay

--- tdmpc2/drS/modem_trainer.py
+++ tdmpc2/drS/modem_trainer.py
@@ -19,6 +19,8 @@ class ModemTrainer(Trainer):
 		self._pretrain_step = 0
 		self._ep_idx = 0
 		self._start_time = time()
+		self._alpha = 1
+		self._alpha_decay = 1e-4
 
 	def common_metrics(self):
 		"""Return a dictionary of current metrics."""
@@ -86,11 +88,11 @@ class ModemTrainer(Trainer):
 		n_iterations = self.cfg.pretrain.n_epochs
 		self.cfg.pretrain.eval_freq = n_iterations // 25
 		start_time = time()
-		best_model, best_score = deepcopy(self.agent.model.state_dict()), 0
+		best_model, best_score = deepcopy(self.agent.bc_model.state_dict()), 0
 
 		print(colored(f"Policy pretraining: {n_iterations} iterations", "red", attrs=["bold"]))
 
-		self.agent.model.train()
+		self.agent.bc_model.train()
 		for self._pretrain_step in range (n_iterations):
 			metrics = self.agent.init_bc(demo_buffer)
 
@@ -100,7 +102,7 @@ class ModemTrainer(Trainer):
 				self.logger.log(eval_metrics, category="pretrain")
 
 				if eval_metrics["episode_reward"] > best_score:
-					best_model = deepcopy(self.agent.model.state_dict())
+					best_model = deepcopy(self.agent.bc_model.state_dict())
 					best_score = eval_metrics["episode_reward"]
 					best_seed = eval_metrics["best_seed"]
 			
@@ -109,11 +111,12 @@ class ModemTrainer(Trainer):
 				self.logger.log(metrics, category="pretrain")
 		
 		if best_score == 0:
-			best_model = deepcopy(self.agent.model.state_dict())
+			best_model = deepcopy(self.agent.bc_model.state_dict())
 			best_seed = eval_metrics["best_seed"]
 		
 		self.agent.model.eval()
 		self.agent.model.load_state_dict(best_model)
+		self.agent.bc_model.load_state_dict(best_model)
 		self.seed_scheduler.start(init_seed=best_seed, max_seeds=1e4)
 
 	def train(self):
@@ -163,7 +166,11 @@ class ModemTrainer(Trainer):
 
 			# Collect experience
 			if self._step > self.cfg.seed_steps:
-				action = self.agent.act(obs, t0=len(self._tds)==1)
+				self._alpha = max(0, self._alpha - self._alpha_decay)
+				if np.random.random() < self._alpha and self.cfg.get("policy_pretraining", False):
+					action = self.agent.policy_action(obs, eval_mode=True)
+				else:
+					action = self.agent.act(obs, t0=len(self._tds)==1)
 			elif self.cfg.get("policy_pretraining", False):
 				action = self.agent.policy_action(obs, eval_mode=True)
 			else:
