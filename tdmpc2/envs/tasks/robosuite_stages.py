9f90774c9d0f88cbf64dbafca0f5ef355136fb73
adrialopezescoriza <alopez@ethz.ch>
2024-11-25 20:51:52 -0800

    bug bool robosuite

--- tdmpc2/envs/tasks/robosuite_stages.py
+++ tdmpc2/envs/tasks/robosuite_stages.py
@@ -123,7 +123,9 @@ class RobosuiteTask(gym.Env):
     def step(self, action):
         obs, reward, done, info = self._env.step(action)
         obs = self._unpack_obs(obs)
-        info["success"] = self._env._check_success().item()
+        info["success"] = self._env._check_success()
+        if isinstance(info["success"], np.bool_):
+            info["success"] = info["success"].item()
         return obs, reward, False, False, info
     
     def reward(self):
